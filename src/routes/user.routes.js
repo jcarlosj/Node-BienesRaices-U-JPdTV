f08aa85ecdda325bafbf58ce4430eba238758138
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-06-22 14:01:05 -0500

    [+] Router/Views: Pasar datos a las vistas

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,12 +6,16 @@ const router = express.Router();
 // ? Agrupa acciones HTTP sobre la misma ruta
 router.route( '/login' )
     .get( ( request, response ) => {
-        response.render( './auth/login' );
+        response.render( './auth/login', {
+            name_page: 'Login page'
+        });
     });
 
 router.route( '/register' )
     .get( ( request, response ) => {
-        response.render( './auth/register' );
+        response.render( './auth/register', {
+            name_page: 'Register page'
+        });
     });
 
 
