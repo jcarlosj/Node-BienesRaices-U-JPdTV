a802947864a3998fda186bdc2e7a6556ba8f8956
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-06-22 12:08:08 -0500

    [+] NPM: npm i express [+] Routes & Lanza servidor express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log( 'index.js' );
+const
+    PORT = 4000,
+    express = require( 'express' )
+    app = express();
+
+// * Routing
+app.get( '/', ( request, response ) => {
+    response.send( '<h1>Bienes Raices</h1>' );
+});
+
+app.get( '/about-us', ( request, response ) => {
+    response.send( '<h1>Nosotros</h1>' );
+});
+
+app.listen( PORT, () => {
+    console.log( `Listening on port ${ PORT }` );
+});
+
