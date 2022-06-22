b2c60cb52f3b325da630efe1a651606d0a48e067
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-06-22 12:39:24 -0500

    [+] Routes: Agrupacion de acciones HTTP por ruta

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,9 +3,14 @@ import express from 'express';
 const router = express.Router();
 
 
-router.get( '/', ( request, response ) => {
-    response.send( '<h1>Bienes Raices</h1>' );
-});
+// ? Agrupa acciones HTTP sobre la misma ruta
+router.route( '/' )
+    .get( ( request, response ) => {
+        response.json({ path: '/', method: 'GET' });
+    })
+    .post( ( request, response ) => {
+        response.json({ path: '/', method: 'POST' });
+    });
 
 router.get( '/about-us', ( request, response ) => {
     response.send( '<h1>Nosotros</h1>' );
