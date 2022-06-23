bf0d1333be0e2c3f833dc7c0f5043554d86389a3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-06-23 12:11:39 -0500

    [+] NPM: npm i dotenv e Implementacion en configuracion de BD

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,9 +1,12 @@
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize( 'bienesraices', 'user_bienesraices', 'pass_bienesraices', {
-    host: 'localhost',
-    port: 3306,
+const sequelize = new Sequelize( process.env.DB_NAME, process.env.DB_USER ?? 'root', process.env.DB_PASS ?? '', {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',       // ? Sequelize soporta varios motores de bases de datos
     // ! Habilita campos
     define: {
