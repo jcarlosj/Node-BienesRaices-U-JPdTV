b2983cc6eade6998ef0d38911675173dff4ec8d6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-07-13 09:14:45 -0500

    [+] Webpack: add-image.js [+] View: Implementa formulario Dropzone

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ import path from 'path';
 export default {
     mode: 'development',
     entry: {
-        map: './src/assets/js/map.js'
+        map: './src/assets/js/map.js',
+        add_image: './src/assets/js/add-image.js'
     },
     output: {
         filename: '[name].js',
         path: path.resolve( 'public/js' )
     },
-}
+} 
