38bcfad6c7d18751ccf358feed8f76515f919dc0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-07-13 09:42:58 -0500

    [+] Seed: Nuevo usuario de prueba

--- src/seed/users.js
+++ src/seed/users.js
@@ -4,8 +4,14 @@ const PASS_DEFAULT = 'pokecito';
 
 const users = [
     { 
-        name: 'Juan',
+        name: 'Juan C Jimenez G',
         email: 'jcjimenez29@misena.edu.co',
+        confirmed: 1,
+        password: bcrypt.hashSync( PASS_DEFAULT, 10 )
+    },
+    { 
+        name: 'Sofia Gutierrez G',
+        email: 'sofia@correo.co',
         confirmed: 1,
         password: bcrypt.hashSync( PASS_DEFAULT, 10 )
     }
